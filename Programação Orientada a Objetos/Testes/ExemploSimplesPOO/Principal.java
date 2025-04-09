afacd605262644f5a18c15061e3e0f2cb8e5008e
Pedro Enzo <penzoluna@gmail.com>
2025-04-08 21:32:58 -0300

    Commitando primeiro codigo em objetos

---
+++ Programação Orientada a Objetos/Testes/ExemploSimplesPOO/Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+	public static void main (String[]args){
+	
+		MoldeCarro meucarro = new MoldeCarro( "Troller T4","Troller", 2015);
+
+		meucarro.PrintInfo();
+
+		meucarro.Fazervrumvrum();
+
+	}
+}
