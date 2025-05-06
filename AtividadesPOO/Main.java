95890dcc2af2e1a32679140e470a41d1491af675
Pedro Enzo <165727452+PedroProgramador86@users.noreply.github.com>
2025-05-06 13:56:52 +0000

    Commitando pelo CodeSpace

---
+++ AtividadesPOO/Main.java
@@ -0,0 +1,20 @@
+public class Main{
+
+    public static void main (String[]args){
+        
+        List <String> lista = new ArrayList<>();
+        lista.add("Hello World 1");
+        lista.add("Hello World 2");
+        lista.add("Hello World 3");
+        lista.add("Hello World 4");
+
+        // Percorrendao a lista: 
+
+        for(String variavelDeControle : lista){
+
+            System.out.println(lista);
+
+        }
+
+    }
+}
