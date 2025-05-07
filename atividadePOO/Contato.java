9115b64ea925cdb5a0301079e663bbcd4484cf0b
Pedro Enzo <bazukastream8866@gmail.com>
2025-05-07 00:33:25 -0300

    Adicionando commit de modificação

--- atividadePOO/Contato.java
+++ atividadePOO/Contato.java
@@ -1,21 +1,41 @@
 package atividadePOO;
 
 public class Contato {
-    protected String nome;
-    protected String telefone;
-    protected String email;
 
-    public Contato(String nome, String telefone, String email) {
+    protected String nome;          // Variaveis
+    protected String telefone;      //    de
+    protected String email;         // Instancia
+
+    public Contato(String nome, String telefone, String email) { // Construtor
+
         this.nome = nome;
         this.telefone = telefone;
         this.email = email;
+    
     }
 
-    public String getNome() { return nome; }
-    public String getTelefone() { return telefone; }
-    public String getEmail() { return email; }
+    // Retornando Valores bloqueados pelo "protected" das variaveis de instancia
+
+    public String getNome() { 
+    
+        return nome; 
+    
+    }
+    
+    public String getTelefone() { 
+    
+        return telefone; 
+    
+    }
+    
+    public String getEmail() { 
+    
+        return email; 
+    
+    }
 
     public String getDetalhes() {
         return "Nome: " + nome + ", Telefone: " + telefone + ", Email: " + email;
+    
     }
 }
