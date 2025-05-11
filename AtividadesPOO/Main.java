c10ec1c4b36df76bc3719d82a2677f8efff565ed
Pedro Enzo <bazukastream8866@gmail.com>
2025-05-11 18:04:07 -0300

    Commit

---
+++ AtividadesPOO/Main.java
@@ -0,0 +1,25 @@
+package AtividadesPOO.ApresentacaoCodeSpace;
+
+import java.util.*;
+
+public class Main{
+
+
+    public static void main (String[]args){
+        
+        List <String> lista = new ArrayList<>();
+        lista.add("Hello World 1");
+        lista.add("Hello World 2");
+        lista.add("Hello World 3");
+        lista.add("Hello World 4");
+
+        // Percorrendo a lista: 
+
+        for(String variavelDeControle : lista){
+
+            System.out.println(lista);
+
+        }
+
+    }
+}
